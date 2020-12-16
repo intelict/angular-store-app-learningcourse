144530e2a312039c58ee3c6e284e601f72ad3e48
intelict <sascha.peric@intelict.de>
2020-12-16 14:09:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-xu5p4w-h1updg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xu5p4w-h1updg)
---
+++ src/app/products.ts
@@ -0,0 +1,28 @@
+export const products = [
+  {
+    name: "Phone XL",
+    price: 799,
+    description: "A large phone with one of the best screens"
+  },
+  {
+    name: "Phone Mini",
+    price: 701,
+    description: "A great phone with one of the best cameras"
+  },
+  {
+    name: "Phone Standard",
+    price: 900,
+    description: ""
+  },
+  {
+    name: "Phone 4 XL",
+    price: 349,
+    description: "The best phone I ever had"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
